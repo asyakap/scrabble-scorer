4076de0a46abd8bb4a16ff448cde390c11a89f08
Asia Kaplanyan <a.kaplanyan@gmail.com>
2023-02-22 10:45:28 -0800

    pass the test for scores

--- ScrabbleScorer.Tests/ModelTests/ScrabbleTests.cs
+++ ScrabbleScorer.Tests/ModelTests/ScrabbleTests.cs
@@ -1,21 +1,28 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ScrabbleScorer.Models;
 using System.Collections.Generic;
-using System;
 
 namespace ScrabbleScorer.Tests
 {
   [TestClass]
-  public class ScrabbleTests
+  public class GameTests
   {
+
+    
     [TestMethod]
-    public void ScrabbleConstructor_CreatesInstanceOfScrabbleWord_ScrabbleWords()
+    public void LetterScore_ReturnsScoreForSingleLetter_Int()
     {
-      ScrabbleWords c = new ScrabbleWords(); // 
-      Assert.AreEqual(typeof(ScrabbleWords), c.GetType());
-    }
+      char input = 'a';
+      int result = ScrabbleScore.LetterScore(input);
+      Assert.AreEqual(1, result);
+    } //code that passes switch case -- test was re
 
-
-
+    [TestMethod]
+    public void WordScore_ReturnsScoreForEntireWord_Int()
+    {
+      string input = "hi";
+      int result = ScrabbleScore.WordScore(input);
+      Assert.AreEqual(5,result);
+    } //testing return of score for string. test that runs through switch case every instance of letter appearing in word and adds them
   }
 }
